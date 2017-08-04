8b10da9162132c786ebf898f991b67c85d4b2335
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-08-04 17:25:58 +0100

    Finish functions

---
+++ pet_shop.rb
@@ -0,0 +1,80 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, cash)
+  pet_shop[:admin][:total_cash] = total_cash(pet_shop) + cash
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, sold_pets)
+  pet_shop[:admin][:pets_sold] += sold_pets
+end
+
+def stock_count (pet_shop)
+  return pet_shop[:pets].length
+end
+
+def pets_by_breed(pet_shop, breed)
+this_breed = []
+  for pet in pet_shop[:pets]
+    if pet[:breed] == breed
+      this_breed.push(pet)
+    end
+  end
+  return this_breed
+end
+
+def find_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+        return pet
+    end
+  end
+  return nil
+end
+
+def remove_pet_by_name(pet_shop, name)
+  arr = pet_shop[:pets]
+  arr.delete_if { |pet| pet[:name] == name}
+  return find_pet_by_name(pet_shop, name)
+end
+
+def add_pet_to_stock(pet_shop, new_pet)
+ pet_shop[:pets].push(new_pet)
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].count
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+end
+
+def customer_can_afford_pet(customer, new_pet)
+  if (customer[:cash] <= new_pet[:price])
+    return false
+  else
+    return true
+  end
+end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  if pet == nil 
+    customer[:pets].count
+  elsif customer[:cash] < pet[:price]
+    customer[:pets].count
+  else
+    customer[:pets].push(pet)
+    pet_shop[:admin][:pets_sold] += 1
+    pet_shop[:admin][:total_cash] += pet[:price]
+  end
+end
